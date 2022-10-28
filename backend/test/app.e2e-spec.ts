c9fe5046fa22244d572e63d6d36593e916901c19
sorena <sorenapaydar81@gmail.com>
2022-10-28 16:20:17 +0330

    test: implement e2e test

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,24 +1,131 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/app/prisma/prisma.service';
+import { LoginDto, RegisterDto } from '../src/app/auth/dto';
 
-describe('AppController (e2e)', () => {
+describe('App (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+
+    // Global pipes
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+
     await app.init();
+    await app.listen(3333);
+
+    // Prisma Service
+    prisma = app.get(PrismaService);
+
+    // Clean database
+    await prisma.cleanDb();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('Auth', () => {
+    const registerDto: RegisterDto = {
+      email: 'sorena@gmail.com',
+      username: 'sorena',
+      password: 'strongpassword',
+    };
+
+    describe('Register', () => {
+      it('create new user', (done) => {
+        request(app.getHttpServer())
+          .post('/auth/register')
+          .send(registerDto)
+          .expect(HttpStatus.CREATED)
+          .end((err, res) => {
+            if (err) return done(err);
+
+            return done();
+          });
+      });
+
+      it('throw exception if no credentials provided', (done) => {
+        request(app.getHttpServer())
+          .post('/auth/register')
+          .send({})
+          .expect(HttpStatus.BAD_REQUEST)
+          .end((err, res) => {
+            if (err) return done(err);
+
+            return done();
+          });
+      });
+
+      it('throw exception if credentials taken', (done) => {
+        request(app.getHttpServer())
+          .post('/auth/register')
+          .send(registerDto)
+          .expect(HttpStatus.FORBIDDEN)
+          .end((err, res) => {
+            if (err) return done(err);
+
+            return done();
+          });
+      });
+    });
+
+    describe('Login', () => {
+      const loginDto: LoginDto = {
+        email: registerDto.email,
+        password: registerDto.password,
+      };
+
+      it('login to the created user', (done) => {
+        request(app.getHttpServer())
+          .post('/auth/login')
+          .send(loginDto)
+          .expect(HttpStatus.OK)
+          .end((err, res) => {
+            if (err) return done(err);
+
+            return done();
+          });
+      });
+
+      it('throw exception if no credentials provided', (done) => {
+        request(app.getHttpServer())
+          .post('/auth/login')
+          .send({})
+          .expect(HttpStatus.BAD_REQUEST)
+          .end((err, res) => {
+            if (err) return done(err);
+
+            return done();
+          });
+      });
+
+      it('throw exception if credentials incorrect', (done) => {
+        request(app.getHttpServer())
+          .post('/auth/login')
+          .send({
+            email: loginDto.email,
+            password: 'differentpassword',
+          })
+          .expect(HttpStatus.UNAUTHORIZED)
+          .end((err, res) => {
+            if (err) return done(err);
+
+            return done();
+          });
+      });
+    });
   });
 });
