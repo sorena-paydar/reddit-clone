231f9fdae5e9bb90bd7cb109fc26646b08d77510
sorena <sorenapaydar81@gmail.com>
2022-10-31 23:58:22 +0330

    style: fix typo

--- backend/src/app/auth/auth.service.ts
+++ backend/src/app/auth/auth.service.ts
@@ -74,7 +74,7 @@ export class AuthService {
     const secret = this.config.get('JWT_SECRET');
 
     const token = await this.jwt.signAsync(payload, {
-      expiresIn: '2m',
+      expiresIn: '30d',
       secret,
     });
 
--- backend/src/app/auth/decorator/get-user.decorator.ts
+++ backend/src/app/auth/decorator/get-user.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 
-export const getUser = createParamDecorator(
+export const GetUser = createParamDecorator(
   (data: string | undefined, ctx: ExecutionContext) => {
     const request: Request = ctx.switchToHttp().getRequest();
 
