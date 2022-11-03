d1d353f0e7d4407f266a9c0d80a25ea1e197594d
sorena <sorenapaydar81@gmail.com>
2022-11-03 23:35:23 +0330

    feat: add public decorator to main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { JwtGuard } from './app/auth/guard/jwt.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtGuard(reflector));
 
   app.useGlobalPipes(
     new ValidationPipe({
