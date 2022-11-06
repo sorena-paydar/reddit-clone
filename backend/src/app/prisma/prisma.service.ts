91ab3923c428bfd51eb06094f77a1fecade1cd14
sorena <sorenapaydar81@gmail.com>
2022-11-06 14:00:58 +0330

    test: update cleanDb func

--- backend/src/app/prisma/prisma.service.ts
+++ backend/src/app/prisma/prisma.service.ts
@@ -15,6 +15,9 @@ export class PrismaService extends PrismaClient {
   }
 
   cleanDb() {
-    return this.$transaction([this.user.deleteMany()]);
+    return this.$transaction([
+      this.user.deleteMany(),
+      this.subreddit.deleteMany(),
+    ]);
   }
 }
