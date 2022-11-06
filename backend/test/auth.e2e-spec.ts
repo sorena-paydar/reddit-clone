b30d3ba79075b2389e1f6a2f5ba4dd373d3da07a
sorena <sorenapaydar81@gmail.com>
2022-11-06 14:01:53 +0330

    test: update auth e2e test

--- backend/test/auth.e2e-spec.ts
+++ backend/test/auth.e2e-spec.ts
@@ -11,13 +11,6 @@ const authDto: RegisterDto = {
   password: 'strongpassword',
 };
 
-enum ERROR_MESSAGES {
-  credentials_taken = 'Credentials taken',
-  user_not_found = 'User not found',
-  credentials_incorrect = 'Credentials incorrect',
-  bad_request = 'Bad Request',
-}
-
 describe('/auth', () => {
   let app: INestApplication;
   let prisma: PrismaService;
@@ -29,8 +22,6 @@ describe('/auth', () => {
 
     app = moduleRef.createNestApplication();
 
-    console.log(app);
-
     // Global pipes
     app.useGlobalPipes(
       new ValidationPipe({
@@ -39,7 +30,6 @@ describe('/auth', () => {
     );
 
     await app.init();
-    await app.listen(3333);
 
     // Prisma Service
     prisma = app.get(PrismaService);
@@ -63,24 +53,17 @@ describe('/auth', () => {
     });
 
     it('/auth/register POST (404: credentials not provided)', async () => {
-      const { body } = await request(app.getHttpServer())
+      await request(app.getHttpServer())
         .post('/auth/register')
         .send({})
         .expect(HttpStatus.BAD_REQUEST);
-
-      expect(body.error).toEqual(ERROR_MESSAGES.bad_request);
     });
 
     it('/auth/register POST (403: credentials taken)', async () => {
-      const { body } = await request(app.getHttpServer())
+      await request(app.getHttpServer())
         .post('/auth/register')
         .send(authDto)
         .expect(HttpStatus.FORBIDDEN);
-
-      expect(body).toEqual({
-        success: false,
-        message: ERROR_MESSAGES.credentials_taken,
-      });
     });
   });
 
@@ -95,27 +78,20 @@ describe('/auth', () => {
     });
 
     it('/auth/login POST (404: credentials not provided)', async () => {
-      const { body } = await request(app.getHttpServer())
+      await request(app.getHttpServer())
         .post('/auth/login')
         .send({})
         .expect(HttpStatus.BAD_REQUEST);
-
-      expect(body.error).toEqual(ERROR_MESSAGES.bad_request);
     });
 
     it('/auth/login POST (403: credentials incorrect)', async () => {
-      const { body } = await request(app.getHttpServer())
+      await request(app.getHttpServer())
         .post('/auth/login')
         .send({
           email: authDto.email,
           password: 'differentpassword',
         } as LoginDto)
         .expect(HttpStatus.UNAUTHORIZED);
-
-      expect(body).toEqual({
-        success: false,
-        message: ERROR_MESSAGES.credentials_incorrect,
-      });
     });
   });
 });
